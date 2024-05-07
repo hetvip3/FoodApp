94538c318f76e2948f7fbc9f21efc44c8954a5b9
hetvip3 <hetvipatel3598@gmail.com>
2024-05-07 21:01:13 +0530

    added register,login,logout page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import *
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Welcome {username},Your Account has been successfully created' )
+            return redirect('login')
+
+    else:
+        form = RegisterForm()
+    return render(request,'users/register.html',{'form':form})
